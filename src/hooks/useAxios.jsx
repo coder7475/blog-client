852a7ea4b40216b16824ab2686b47cb5ccbdee14
coder7475 <robiulhossain7475@gmail.com>
2023-11-06 23:27:36 +0600

    feat: create useAxios hook

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: "https://server-programmers-blog.vercel.app/api/v1",
+  withCredentials: true,
+})
+
+const useAxios = () => {
+  return instance;
+};
+
+export default useAxios;
