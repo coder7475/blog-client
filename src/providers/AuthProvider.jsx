a6ae4b9dfb39275e382231ebb4e41364c9018c35
coder7475 <robiulhossain7475@gmail.com>
2023-11-07 07:58:42 +0600

    feat: show sweet alert for google sign in

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,12 +1,10 @@
 import PropTypes from "prop-types";
-import {
-  onAuthStateChanged,
-  signInWithPopup,
-} from "firebase/auth";
+import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import { GoogleAuthProvider, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
-import AuthContext from '../contexts/AuthContext';
-import auth from '../Firebase/firebase.config';
+import AuthContext from "../contexts/AuthContext";
+import auth from "../Firebase/firebase.config";
+import Swal from "sweetalert2";
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
@@ -14,34 +12,49 @@ function AuthProvider({ children }) {
   // console.log(user);
 
   useEffect(() => {
-    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       // console.log("user in the auth state changed", currentUser);
       setUser(currentUser);
       setLoading(false);
     });
     return () => unSubscribe();
-  }, []);  
+  }, []);
 
   // implement google sign in
   const provider = new GoogleAuthProvider();
   const googleSignIn = () => {
     setLoading(true);
+
     return signInWithPopup(auth, provider)
-      .then((res) => alert(res))
-      .catch((err) => alert(err));
+      .then(() => {
+        Swal.fire({
+          title: "Success!",
+          text: "You have successfully logged in!",
+          icon: "success",
+        })
+        
+      }
+      )
+      .catch(() =>
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        })
+      );
   };
 
   const logOut = () => {
     setUser(null);
     setLoading(true);
     return signOut(auth);
-  }
+  };
 
   const authInfo = {
     user,
     loading,
     googleSignIn,
-    logOut
+    logOut,
   };
 
   return (
