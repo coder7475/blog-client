58062b65c429ea635472c2c31dd40de389982a3c
coder7475 <robiulhossain7475@gmail.com>
2023-11-08 15:07:55 +0600

    feat: show all blogs added to wishlist for current user

---
+++ src/components/WishCard.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types"
+import { NavLink } from "react-router-dom";
+
+const WishCard = ({ data }) => {
+  // console.log(data);
+  const { blog_id, _id, title, category, image, short_description, timestamp, user_email } = data;
+  return (
+    <div className="card justify-center items-center shadow-xl border-2 border-gray-400 bg-slate-600">
+     <figure className="p-5 w-full">
+        <img src={image} alt="service" className="rounded-xl h-64 w-full brightness-75" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{title}</h2>
+        <h2 className="card-title font-thin">Published: {timestamp.slice(0, 10)}     {timestamp.slice(11,16)}</h2>
+        <h2 className="text-xl">
+          {/* <span className="font-semibold">Category: </span> */}
+          {category}
+        </h2>
+
+        <p className="text-xl py-2 font-light">{short_description}</p>
+        <div className="card-actions">
+          <button className="btn text-white text-lg font-semibold">
+            <NavLink to={`/${blog_id}`}>View Details</NavLink>
+          </button>
+          <button className="btn text-white text-lg font-semibold">
+            Remove to Wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+WishCard.propTypes = {
+  data: PropTypes.shape({
+    _id: PropTypes.any,
+    blog_id: PropTypes.any,
+    category: PropTypes.any,
+    image: PropTypes.any,
+    short_description: PropTypes.any,
+    timestamp: PropTypes.any,
+    title: PropTypes.any,
+    user_email: PropTypes.any
+  })
+}
+
+export default WishCard;
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,11 +1,43 @@
-
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
+import useAxios from "../hooks/useAxios";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import WishCard from "../components/WishCard";
 
 const Wishlist = () => {
+  const { user } = useContext(AuthContext);
+  // console.log(user.email);
+  const mainAxios = useAxios();
+  const url = `/user/wishlist?userMail=${user.email}`;
+  // console.log(url);
+  const getAllWishes = async () => {
+    const res = await mainAxios.get(url);
+    return res;
+  };
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["allWishes"],
+    queryFn: getAllWishes,
+  });
+
+  if (isLoading) {
+    return <span>Loading....</span>;
+  }
+
+  // console.log(data);
+
   return (
     <main>
-      <Navbar/>
-      WishList      
+      <Navbar />
+      <h1 className="text-5xl text-center font-bold my-10">Your Wishlist</h1>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 max-w-6xl mx-auto my-10">
+        {data.data.map((wish) => (
+          <WishCard key={wish._id} data={wish}>
+            {wish.title}
+          </WishCard>
+        ))}
+      </section>
     </main>
   );
 };
